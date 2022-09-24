0d7188fb35f536aa94c9445c6779a177701eed3b
Ramil Abbasov <abbasov.ram3107@gmail.com>
2022-09-24 14:28:33 +0300

    prepare favourites screen

--- src/screens/favourites/Favourites.js
+++ src/screens/favourites/Favourites.js
@@ -1,12 +1,48 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Alert, FlatList } from 'react-native';
+import { CenteredActivityIndicator } from '../../components/CenteredActivityIndicator';
+import { SearchBarCustom } from '../../components/SearchBar'
+import { FavouriteRecipeCard } from './components/FavouriteRecipeCard';
 
 export const Favourites = () => {
+
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [favouriteRecipes, setFavouriteRecipes] = useState(true);
+
+    const getFavouritesRecipes = async () => {
+        try {
+            setIsLoading(true)
+            const response = await axios.get("https://6321d90d82f8687273baba5a.mockapi.io/enjoy/recipes")
+            const data = response.data
+            setFavouriteRecipes(data)
+            setIsLoading(false)
+        } catch (error) {
+            Alert.alert(error.message)
+            setIsLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        getFavouritesRecipes()
+    }, [])
+
+
     return (
-        <View>
-            <Text>
-                Favoutites
-            </Text>
-        </View>
+        <>
+
+            {isLoading && <CenteredActivityIndicator />}
+
+            {!isLoading && <>
+                <SearchBarCustom />
+
+                <FavouriteRecipeCard favouriteRecipes={favouriteRecipes} />
+            </>
+            }
+
+        </>
     )
 }
+
+
---
+++ src/screens/favourites/components/FavouriteRecipeCard.js
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import { ActivityIndicator, FlatList, StyleSheet, Text } from "react-native";
+import { Image, TouchableOpacity } from "react-native"
+import Icon from 'react-native-vector-icons/Ionicons';
+
+
+export const FavouriteRecipeCard = ({ favouriteRecipes }) => {
+
+    const [isFavouriteIconSelected, setIsFavouriteIconSelected] = useState(false);
+
+    return (
+
+        <FlatList
+            data={favouriteRecipes}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => {
+                return (
+                    <TouchableOpacity
+                        activeOpacity={0.8}
+                        style={styles.recipeCardWrapper}>
+                        <Image
+                            PlaceholderContent={<ActivityIndicator />}
+                            borderRadius={15}
+                            style={styles.recipeCardImage}
+                            source={{ uri: item.image }}
+                        />
+                        <Icon
+                            onPress={() => console.log("Selected")}
+                            style={styles.recipeCardIcon}
+                            name={
+                                isFavouriteIconSelected
+                                    ? 'md-heart-sharp'
+                                    : "md-heart-outline"
+                            }
+                            size={40}
+                            color="white"
+                        />
+                        <Text
+                            style={styles.recipeCardText}>
+                            {item.dish}
+                        </Text>
+                    </TouchableOpacity>
+                )
+            }}
+        />
+    )
+}
+
+
+const styles = StyleSheet.create({
+    recipeCardWrapper: {
+        alignItems: "center",
+        marginBottom:30
+    },
+    recipeCardImage: {
+        height: 300,
+        width: "90%",
+    },
+    recipeCardIcon: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 1,
+        shadowRadius: 2,
+        elevation: 10,
+        position: "absolute",
+        left: "77%",
+        top: "8%"
+    },
+    recipeCardText: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 1,
+        shadowRadius: 2,
+        elevation: 10,
+        color: "white",
+        fontWeight: "600",
+        fontSize: 30,
+        position: "absolute",
+        top: "65%",
+        marginHorizontal: 15,
+    }
+})
